f762b0ea11ebb652a7020bae6d7e16694f8e22db
mescoubas-tech <mescoubas@gmail.com>
2025-08-20 18:51:00 +0200

    Update config.py

--- app/plannings/config.py
+++ app/plannings/config.py
@@ -2,7 +2,6 @@
 from pydantic import BaseModel, Field
 
 class RuleSettings(BaseModel):
-    # Heures & repos
     max_daily_hours: float = Field(10.0)
     max_daily_hours_with_derog: float = Field(12.0)
     max_weekly_hours: float = Field(48.0)
@@ -21,5 +20,4 @@ class RuleSettings(BaseModel):
 class Settings(BaseModel):
     rules: RuleSettings = RuleSettings()
 
-# >>> IMPORTANT : on expose bien une instance globale
 SETTINGS = Settings()
